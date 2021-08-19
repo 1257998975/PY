86fb4245aa1941c534a7717f187f6b4c589734cc
smart <1257998975@qq.com>
2021-08-19 18:00:57 +0800

    添加背景音乐

--- main.py
+++ main.py
@@ -2,35 +2,47 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.core.audio import SoundLoader
+from kivy.core.text import LabelBase
+from kivy.core.image import Image as CoreImage
+
+# -*- coding: utf-8 -*-
+
+
+LabelBase.register(name='Font_Hanzi', fn_regular='./fonts/STKAITI.ttf')
 
 Builder.load_string('''
 <GridLayout>
     canvas.before:
         BorderImage:
             # BorderImage behaves like the CSS BorderImage
-            border: 10, 10, 10, 10
+            border: 0, 0, 0, 0
             source: 'image/1.jpg'
             pos: self.pos
             size: self.size
 <RootWidget>
     GridLayout:
-        size_hint: .9, .9
+        # size_hint: .9, .9
         pos_hint: {'center_x': .5, 'center_y': .5}
         rows:1
+        # Label:
+        #     text: "I don't suffer from insanity, I enjoy every minute of it"
+        #     text_size: self.width-20, self.height-20
+        #     valign: 'top'
+        # Label:
+        #     text: "When I was born I was so surprised; I didn't speak for a year and a half."
+        #     text_size: self.width-20, self.height-20
+        #     valign: 'middle'
+        #     # halign: 'center'
         Label:
-            text: "I don't suffer from insanity, I enjoy every minute of it"
-            text_size: self.width-20, self.height-20
-            valign: 'top'
-        Label:
-            text: "When I was born I was so surprised; I didn't speak for a year and a half."
-            text_size: self.width-20, self.height-20
-            valign: 'middle'
-            halign: 'center'
-        Label:
-            text: "A consultant is someone who takes a subject you understand and makes it sound confusing"
-            text_size: self.width-20, self.height-20
+            canvas.before:
+                Rectangle:
+                    pos: [0,0]
+                    size: self.width-20,100
+                    source: 'image/1.png'
+            text: "今天又一不小心看动漫到这么晚了，好想去二次元啊~"
+            text_size: self.width-200, self.height-100
             valign: 'bottom'
-            halign: 'justify'
+            font_name: 'Font_Hanzi'
 ''')
 
 
@@ -41,10 +53,7 @@ class RootWidget(FloatLayout):
 class MainApp(App):
     def build(self):
         sound = SoundLoader.load('music/1.mp3')
-        if sound:
-            print("Sound found at %s" % sound.source)
-            print("Sound is %.3f seconds" % sound.length)
-            sound.play()
+        sound.play()
         return RootWidget()
 
 
